506cbb887f04b4d93b7cbb9d18739370a1c8e93a
Ermalenok Anton <antonermalenok@gmail.com>
2023-01-02 17:36:01 +0300

    addes class Message

---
+++ ConsoleClient/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMessager
+{
+    public class Message
+    {
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public DateTime TimeStmap { get; set; }
+
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Server is running...";
+            TimeStmap = DateTime.Now;
+        }
+
+        public Message(string userName, string messageText, DateTime timeStmap)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStmap = timeStmap;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} <{1}>: {2}", UserName, TimeStmap, MessageText);
+        }
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MyMessager;
+
+Message message = new Message();
+Console.WriteLine(message.ToString());
