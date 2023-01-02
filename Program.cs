2d00ebf7227943c7b9afe8d46c8b0af87191caf8
User <User@WIN-R7M8FCBUPOA>
2023-01-02 16:23:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMessager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
