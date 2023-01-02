ee5253cb0fd20ca0ef92e9d4c6cfe7351a64cec0
Ermalenok Anton <antonermalenok@gmail.com>
2023-01-02 17:50:09 +0300

    serialize

--- ConsoleClient/Message.cs
+++ ConsoleClient/Message.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyMessager
 {
+    [Serializable]
     public class Message
     {
         public string UserName { get; set; }
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MyMessager;
+using Newtonsoft.Json;
 
-Message message = new Message();
-Console.WriteLine(message.ToString());
+Message message = new Message("Anton", "Hello", DateTime.UtcNow);
+string output = JsonConvert.SerializeObject(message);
+Console.WriteLine(output);
+Message deserilazedMes = JsonConvert.DeserializeObject<Message>(output);
+Console.WriteLine(deserilazedMes);
+//Console.WriteLine(message.ToString());
